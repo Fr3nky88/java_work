8760b718c617295b6abb161837931c690b825cbc
Francesco Checchia <frenkeltonx@gmail.com>
2024-02-21 11:01:16 +0100

    Inizio sviluppo esercizio 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
+
+    /**
+     *
+     * Esercizio 1: Una funzione che gioca a nascondino
+     * Scrivi una funzione che gioca a nascondino con l'utente.
+     * La funzione dovrebbe generare un numero casuale tra 1 e 100 e chiedere all'utente di indovinarlo.
+     * Se l'utente indovina il numero, la funzione dovrebbe stampare "Hai vinto!".
+     * Altrimenti, la funzione dovrebbe stampare "Hai sbagliato! Il numero era {numero_casuale}".
+     *
+     *
+     */
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        nascondino(0);
+    }
+
+    public static void nascondino(int numeroDigitato) {
+        int numeroACaso =(int) (Math.random()*100);
+        System.out.println(numeroACaso);
     }
 }
